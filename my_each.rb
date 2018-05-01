3359af878d3c5b0db2f42a0bffc69598e5204cf5
Eric Chrobak <eric@ericchrobak.com>
2018-05-01 03:12:45 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,9 @@
 def my_each(array) # put argument(s) here
   # code here
-  counter = 0
-  while (counter < array.length)
+  i = 0
+  while (i < array.length)
     yield(array[i])
-    counter += 1
+    i += 1
   end
 end
 collection = [1, 2, 3, 4]
